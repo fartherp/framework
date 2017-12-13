5e928e51256d0fad1e1a172fbd23f85ff84f1c3d
cuiyuqiang@juzix.io <1qazxsw23edc>
2017-12-13 19:43:34 +0800

    添加获取IP

---
+++ framework-common/src/main/java/com/github/fartherp/framework/common/util/IPUtils.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017. juzhen.io. All rights reserved.
+ */
+
+package com.github.fartherp.framework.common.util;
+
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: CK
+ * @date: 2017/12/13
+ */
+public class IPUtils {
+    // 新浪地址
+    public static final String SINA_PATH = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=";
+
+    /**
+     * 获取当前网络IP
+     *
+     * @param request 请求
+     * @return ip
+     */
+    public static String getIP(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (checkIP(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (checkIP(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (checkIP(ip)) {
+            ip = request.getRemoteAddr();
+            if (ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1")) {
+                //根据网卡取本机配置的IP
+                try {
+                    ip = InetAddress.getLocalHost().getHostAddress();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) {
+            //"***.***.***.***".length() = 15
+            ip = StringUtils.substringBefore(ip, ",");
+        }
+        return ip;
+    }
+
+    private static boolean checkIP(String ip) {
+        return ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip);
+    }
+
+    public static RemoteIpInfo getAddress(String ip) {
+        String resp = HttpClientUtils.executeGet(SINA_PATH + ip);
+        return JsonUtil.fromJson(resp, RemoteIpInfo.class);
+    }
+}
---
+++ framework-common/src/main/java/com/github/fartherp/framework/common/util/RemoteIpInfo.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2017. juzhen.io. All rights reserved.
+ */
+
+package com.github.fartherp.framework.common.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: CK
+ * @date: 2017/12/13
+ */
+public class RemoteIpInfo {
+    private String ret;
+    private Integer start;
+    private Integer end;
+    private String country;
+    private String province;
+    private String city;
+    private String district;
+    private String isp;
+    private String type;
+    private String desc;
+
+    public String getRet() {
+        return ret;
+    }
+
+    public void setRet(String ret) {
+        this.ret = ret;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getEnd() {
+        return end;
+    }
+
+    public void setEnd(Integer end) {
+        this.end = end;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getIsp() {
+        return isp;
+    }
+
+    public void setIsp(String isp) {
+        this.isp = isp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("RemoteIpInfo{");
+        sb.append("ret='").append(ret).append('\'');
+        sb.append(", start=").append(start);
+        sb.append(", end=").append(end);
+        sb.append(", country='").append(country).append('\'');
+        sb.append(", province='").append(province).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append(", district='").append(district).append('\'');
+        sb.append(", isp='").append(isp).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append(", desc='").append(desc).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
