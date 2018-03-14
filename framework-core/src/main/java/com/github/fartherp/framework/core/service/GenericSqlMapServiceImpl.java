9153f51135f47e925ef644c2dc43516159d852d8
cuiyuqiang@juzix.io <1qazxsw23edc>
2018-03-14 16:50:16 +0800

    底层增加事务

--- framework-core/src/main/java/com/github/fartherp/framework/core/service/GenericSqlMapServiceImpl.java
+++ framework-core/src/main/java/com/github/fartherp/framework/core/service/GenericSqlMapServiceImpl.java
@@ -9,6 +9,7 @@ import com.github.fartherp.framework.core.dao.SqlMapDao;
 import com.github.fartherp.framework.core.orm.mybatis.plugin.page.Pagination;
 import com.github.fartherp.framework.core.orm.mybatis.plugin.search.vo.Searchable;
 import com.github.fartherp.framework.core.orm.mybatis.plugin.search.vo.Sort;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import java.io.Serializable;
@@ -24,6 +25,7 @@ public abstract class GenericSqlMapServiceImpl<T extends FieldAccessVo, ID exten
 
     public abstract SqlMapDao<T, ID> getDao();
 
+    @Transactional(rollbackFor = {Exception.class})
     @SuppressWarnings("unchecked")
     public void delete(T entity) {
         Assert.notNull(entity, "delete failed due to entity is null");
@@ -38,6 +40,7 @@ public abstract class GenericSqlMapServiceImpl<T extends FieldAccessVo, ID exten
         return getDao().findById(id);
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     @SuppressWarnings("unchecked")
     public ID saveEntity(T entity) {
         Assert.notNull(entity, "save entity failed due to entity is null");
@@ -45,38 +48,46 @@ public abstract class GenericSqlMapServiceImpl<T extends FieldAccessVo, ID exten
         return (ID) entity.getPrimaryKey();
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     @SuppressWarnings("unchecked")
     public ID saveEntitySelective(T entity) {
         Assert.notNull(entity, "save entity failed due to entity is null");
         getDao().saveSelective(entity);
         return (ID) entity.getPrimaryKey();
     }
+
+    @Transactional(rollbackFor = {Exception.class})
     public void deleteBatch(ID[] ids){
         getDao().deleteBatch(ids);
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     public void updateEntity(T entity) {
         Assert.notNull(entity, "update entity failed due to entity is null");
         getDao().update(entity);
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     public void updateEntitySelective(T entity) {
         Assert.notNull(entity, "update entity failed due to entity is null");
         getDao().updateSelective(entity);
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     public T saveOrUpdate(T entity) {
         Assert.notNull(entity, "save or update entity failed due to entity is null");
         getDao().saveOrUpdate(entity);
         return entity;
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     public T saveOrUpdateSelective(T entity) {
         Assert.notNull(entity, "save or update entity failed due to entity is null");
         getDao().saveOrUpdateSelective(entity);
         return entity;
     }
 
+    @Transactional(rollbackFor = {Exception.class})
     public void saveBatch(List<T> entitys) {
         Assert.notNull(entitys, "saveBatch entitys failed due to entitys is null");
         getDao().saveBatch(entitys);
