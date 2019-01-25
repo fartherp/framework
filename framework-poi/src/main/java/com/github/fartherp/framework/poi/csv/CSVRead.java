85093dbb522cb82293de179ff8cb4c3745b5454e
cuiyuqiang@juzix.io <1qazxsw23edc>
2019-01-25 17:02:41 +0800

    csv自定义格式读取

--- framework-poi/src/main/java/com/github/fartherp/framework/poi/csv/CSVRead.java
+++ framework-poi/src/main/java/com/github/fartherp/framework/poi/csv/CSVRead.java
@@ -4,14 +4,18 @@
 
 package com.github.fartherp.framework.poi.csv;
 
+import com.opencsv.CSVParser;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Created by IntelliJ IDEA.
@@ -61,6 +65,85 @@ public class CSVRead<E> {
                 if (i <= deal.skipLine()) {
                     continue;
                 }
+                E o = deal.dealBean(arr);
+                if (o != null) {
+                    l.add(o);
+                    if (i % tmp == 0) {
+                        deal.dealBatchBean(l);
+                        l = new ArrayList<>(tmp);
+                    }
+                }
+            }
+            if (!l.isEmpty()) {
+                deal.dealBatchBean(l);
+            }
+        } catch (IOException e) {
+            // ignore
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    // ignore
+                    inputStream = null;
+                }
+            }
+        }
+    }
+
+    /**
+     * <p>
+     * Example code:
+     * </p>
+     * <pre>
+     * CSVRead<CsvReadDto> csvRead = new CSVRead<>();
+     * CSVParser parser = new CSVParserBuilder().withSeparator('|').withIgnoreQuotations(true).build();
+     * csvRead.read(CSVReadTest.class.getResourceAsStream("/a.csv"), new CSVReadDeal<CsvReadDto>() {
+     *     // 单条数据处理（每个excel一行对应一个javabean）
+     *     public CsvReadDto dealBean(String[] arr) {
+     *         CsvReadDto dto = new CsvReadDto();
+     *         dto.setId(Long.valueOf(arr[0]));
+     *         dto.setName(arr[1]);
+     *         dto.setAge(Integer.valueOf(arr[2]));
+     *         return dto;
+     *     }
+     *
+     *     // 批量数据处理（可以批量入库）
+     *     public void dealBatchBean(List<CsvReadDto> list) {
+     *         Assert.assertEquals("name1", list.get(0).getName());
+     *         Assert.assertEquals("name2", list.get(1).getName());
+     *         Assert.assertEquals("name3", list.get(2).getName());
+     *     }
+     * }, parser);
+     * </pre>
+     *
+     * @param inputStream the file InputStream
+     * @param deal the user-defined of CSVReadDeal
+     *
+     * @see <a href="https://github.com/fartherp/framework/blob/master/framework-poi/src/test/resources/a1.csv">
+     *     file content</a>
+     */
+    public void read(InputStream inputStream, CSVReadDeal<E> deal, CSVParser parser) {
+        this.read(inputStream, deal, reader -> new CSVReaderBuilder(reader)
+                .withSkipLines(deal.skipLine()).withCSVParser(parser).build());
+    }
+
+    /**
+     * user-defined format
+     * @param inputStream the file InputStream
+     * @param deal the user-defined of CSVReadDeal
+     * @param function the function for get Reader
+     * @see {@link #read(java.io.InputStream, com.github.fartherp.framework.poi.csv.CSVReadDeal, com.opencsv.CSVParser)}
+     */
+    public void read(InputStream inputStream, CSVReadDeal<E> deal, Function<Reader, CSVReader> function) {
+        try (CSVReader reader = function.apply(new InputStreamReader(new DataInputStream(inputStream)))) {
+            int tmp = deal.getBatchCount();
+            List<E> l = new ArrayList<>(tmp);
+            int i = deal.skipLine();
+            String[] arr;
+            while ((arr = reader.readNext()) != null) {
+                ++i;
                 E o = deal.dealBean(arr);
                 if (o != null) {
                     l.add(o);
--- framework-poi/src/test/java/com/github/fartherp/framework/poi/csv/CSVReadTest.java
+++ framework-poi/src/test/java/com/github/fartherp/framework/poi/csv/CSVReadTest.java
@@ -4,6 +4,8 @@
 
 package com.github.fartherp.framework.poi.csv;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,7 +19,7 @@ import java.util.List;
  */
 public class CSVReadTest {
     @Test
-    public void testRead() throws Exception {
+    public void testRead() {
         CSVRead<CsvReadDto> csvRead = new CSVRead<>();
         csvRead.read(CSVReadTest.class.getResourceAsStream("/a.csv"), new CSVReadDeal<CsvReadDto>() {
             // 单条数据处理（每个excel一行对应一个javabean）
@@ -38,4 +40,26 @@ public class CSVReadTest {
         });
     }
 
+    @Test
+    public void testRead1() {
+        CSVRead<CsvReadDto> csvRead = new CSVRead<>();
+        CSVParser parser = new CSVParserBuilder().withSeparator('|').withIgnoreQuotations(true).build();
+        csvRead.read(CSVReadTest.class.getResourceAsStream("/a1.csv"), new CSVReadDeal<CsvReadDto>() {
+            // 单条数据处理（每个excel一行对应一个javabean）
+            public CsvReadDto dealBean(String[] arr) {
+                CsvReadDto dto = new CsvReadDto();
+                dto.setId(Long.valueOf(arr[0]));
+                dto.setName(arr[1]);
+                dto.setAge(Integer.valueOf(arr[2]));
+                return dto;
+            }
+
+            // 批量数据处理（可以批量入库）
+            public void dealBatchBean(List<CsvReadDto> list) {
+                Assert.assertEquals("name1", list.get(0).getName());
+                Assert.assertEquals("name2", list.get(1).getName());
+                Assert.assertEquals("name3", list.get(2).getName());
+            }
+        }, parser);
+    }
 }
