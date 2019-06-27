f18e8be0bd23ba8a10acd2f445e4b4c089d8cdc9
fartherp <214722930@qq.com>
2019-06-27 17:37:47 +0800

    修改framework-base-database模块

--- framework-base-database/src/test/java/com/github/fartherp/framework/database/dao/UserMapper.java
+++ framework-base-database/src/test/java/com/github/fartherp/framework/database/dao/UserMapper.java
@@ -5,14 +5,11 @@
 package com.github.fartherp.framework.database.dao;
 
 import com.github.fartherp.framework.database.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 /**
  * `user` 
  */
-@Mapper
 public interface UserMapper extends DaoMapper<User, Integer> {
 
-    User findByUsername(@Param("userName") String userName);
+    User findByUsername(String userName);
 }
