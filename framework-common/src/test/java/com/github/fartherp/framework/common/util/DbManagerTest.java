1456905e20a128ca477b671c2f33915e6b3090de
fartherp <214722930@qq.com>
2019-06-27 17:32:19 +0800

    1.添加mysql环境配置 2.添加DbManagerTest

---
+++ framework-common/src/test/java/com/github/fartherp/framework/common/util/DbManagerTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2019. CK. All rights reserved.
+ */
+
+package com.github.fartherp.framework.common.util;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: CK
+ * @date: 2019/6/27
+ */
+public class DbManagerTest {
+
+	private DbManager dbManager;
+
+	@BeforeMethod
+	public void setUp() {
+		dbManager = new DbManager();
+		dbManager.setDriveName("com.mysql.jdbc.Driver");
+		dbManager.setUser("root");
+		dbManager.setPassword("root");
+		dbManager.setUrl("jdbc:mysql://localhost:3306/test_tmp");
+	}
+
+	@Test
+	public void testGetConnection() {
+		Connection connection = dbManager.getConnection();
+		assertNotNull(connection);
+		dbManager.close(connection);
+	}
+
+	@Test
+	public void testGetPreparedStatement() {
+		Connection connection = dbManager.getConnection();
+		PreparedStatement preparedStatement = dbManager.getPreparedStatement(connection, "select count(1) as count from tb_user");
+		assertNotNull(preparedStatement);
+		dbManager.close(connection, preparedStatement, null);
+	}
+
+	@Test
+	public void testGetResultSet() throws Exception {
+		Connection connection = dbManager.getConnection();
+		PreparedStatement preparedStatement = dbManager.getPreparedStatement(connection, "select count(1) as count from tb_user");
+		ResultSet resultSet = dbManager.getResultSet(preparedStatement);
+		assertNotNull(resultSet);
+		long count = resultSet.getLong("");
+		assertEquals(count, 0);
+		dbManager.close(connection, preparedStatement, resultSet);
+	}
+
+	@Test
+	public void testGetTables() throws Exception {
+		Connection connection = dbManager.getConnection();
+		ResultSet resultSet = dbManager.getTables(connection, "", "test_tmp", "");
+		assertNotNull(resultSet);
+		int size = 0;
+		while (resultSet.next()) {
+			size++;
+		}
+		assertEquals(size, 1);
+		dbManager.close(connection, null, resultSet);
+	}
+
+	@Test
+	public void testGetColumns() throws Exception {
+		Connection connection = dbManager.getConnection();
+		ResultSet resultSet = dbManager.getColumns(connection, "", "test_tmp", "tb_user");
+		assertNotNull(resultSet);
+		int size = 0;
+		while (resultSet.next()) {
+			size++;
+		}
+		assertEquals(size, 6);
+		dbManager.close(connection, null, resultSet);
+	}
+
+	@Test
+	public void testGetPrimaryKeys() throws Exception {
+		Connection connection = dbManager.getConnection();
+		ResultSet resultSet = dbManager.getColumns(connection, "", "test_tmp", "tb_user");
+		assertNotNull(resultSet);
+		int size = 0;
+		while (resultSet.next()) {
+			size++;
+		}
+		assertEquals(size, 1);
+		dbManager.close(connection, null, resultSet);
+	}
+
+	@Test
+	public void testGetResultSetMetaData() throws Exception {
+		Connection connection = dbManager.getConnection();
+		PreparedStatement preparedStatement = dbManager.getPreparedStatement(connection, "select count(1) as count from tb_user");
+		ResultSet resultSet = dbManager.getResultSet(preparedStatement);
+		ResultSetMetaData resultSetMetaData = dbManager.getResultSetMetaData(resultSet);
+		assertNotNull(resultSetMetaData);
+		assertEquals(resultSetMetaData.getColumnCount(), 1);
+	}
+
+	@Test
+	public void testGetDatabaseMetaData() throws Exception {
+		Connection connection = dbManager.getConnection();
+		DatabaseMetaData databaseMetaData = dbManager.getDatabaseMetaData(connection);
+		assertNotNull(databaseMetaData);
+		assertEquals(databaseMetaData.getDatabaseProductName().toLowerCase(), "mysql");
+		dbManager.close(connection);
+	}
+}
