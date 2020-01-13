022582693f60fd9b20490fa69a6f1461cd1b202e
fartherp <214722930@qq.com>
2020-01-13 14:59:31 +0800

    打开sonar脚本注解

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -42,16 +42,14 @@ if [ $TRAVIS_REPO_SLUG == "fartherp/framework" ] && [ "$TRAVIS_BRANCH" == "maste
     ./mvnw clean test jacoco:report coveralls:report -q --settings ./travis/settings.xml
     echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
 
-    ./mvnw sonar:sonar -Dsonar.projectKey=fartherp_framework
-
     # Deploy to site
     # Cannot currently run site this way
     # ./mvnw site site:deploy -q --settings ./travis/settings.xml
     # echo -e "Successfully deploy site under Travis job ${TRAVIS_JOB_NUMBER}"
 
     # Deploy to sonar
-    # ./mvnw clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=a4db6769319225f45af10dfd67e8d982d3f8fe0a -q --settings ./travis/settings.xml
-    # echo -e "Successfully ran Sonar integration under Travis job ${TRAVIS_JOB_NUMBER}"
+    ./mvnw clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.projectKey=fartherp_framework -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=4fc109aa2147f429a3adf9c799f8e73ed92f8337
+    echo -e "Successfully ran Sonar integration under Travis job ${TRAVIS_JOB_NUMBER}"
   else
     echo "Java Version does not support additonal activity for travis CI"
   fi
