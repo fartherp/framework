3fed802e944287dea46f1da97a48dcdcd7d1e20d
fartherp <214722930@qq.com>
2021-03-22 09:21:48 +0800

    excel读取单侧

--- framework-poi/src/test/java/com/github/fartherp/framework/poi/excel/read/ExcelReadTest.java
+++ framework-poi/src/test/java/com/github/fartherp/framework/poi/excel/read/ExcelReadTest.java
@@ -18,8 +18,13 @@ package com.github.fartherp.framework.poi.excel.read;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ExcelReadTest {
 
@@ -46,4 +51,150 @@ public class ExcelReadTest {
             }
         });
     }
+
+    @Test
+    public void testTicai() throws FileNotFoundException {
+		List<ExcelReadA> listAll = new ArrayList<>();
+		ExcelRead.read(new FileInputStream("D:\\1.xls"), new ExcelReadDeal<ExcelReadA>() {
+			// 单条数据处理（每个excel一行对应一个对象）
+			@Override
+			public ExcelReadA dealBean(RowDelegate delegate) {
+				ExcelReadA dto = new ExcelReadA();
+				delegate.getString(0).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::setQishu);
+				delegate.getString(1).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(2).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(3).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(4).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(5).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(6).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(7).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addLan);
+				return dto;
+			}
+
+			// 批量数据处理（可以批量入库）
+			@Override
+			public void dealBatchBean(List<ExcelReadA> list) {
+				listAll.addAll(list);
+			}
+		});
+
+		Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+		listAll.forEach(a -> {
+			List<Integer> lan = a.getLan();
+			lan.forEach(b -> map.compute(b, (k, v) -> {
+				Map<Integer, Integer> count = new HashMap<>();
+				if (v != null) {
+					count.putAll(v);
+				}
+				List<Integer> hong = a.getHong();
+				hong.forEach(c -> count.compute(c, (d, f) -> {
+					if (f == null) {
+						return 1;
+					}
+					return ++f;
+				}));
+				return count;
+			}));
+		});
+
+		Map<Integer, Integer> l1 = map.get(4);
+		Map<Integer, Integer> l2 = map.get(6);
+
+		Map<Integer, Integer> totalCount = new HashMap<>();
+		for (int i = 1; i <= 35; i++) {
+			Integer lv1 = l1.getOrDefault(i, 0);
+			Integer lv2 = l2.getOrDefault(i, 0);
+			totalCount.put(i, lv1 + lv2);
+		}
+
+		System.out.println(1);
+	}
+
+	@Test
+	public void testFucai() throws FileNotFoundException {
+		List<ExcelReadA> listAll = new ArrayList<>();
+		ExcelRead.read(new FileInputStream("D:\\2.xls"), new ExcelReadDeal<ExcelReadA>() {
+			// 单条数据处理（每个excel一行对应一个对象）
+			@Override
+			public ExcelReadA dealBean(RowDelegate delegate) {
+				ExcelReadA dto = new ExcelReadA();
+				delegate.getString(0).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::setQishu);
+				delegate.getString(1).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(2).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(3).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(4).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(5).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(6).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addHong);
+				delegate.getString(7).map(BigDecimal::new).map(BigDecimal::intValue).ifPresent(dto::addLan);
+				return dto;
+			}
+
+			// 批量数据处理（可以批量入库）
+			@Override
+			public void dealBatchBean(List<ExcelReadA> list) {
+				listAll.addAll(list);
+			}
+		});
+
+		Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+		listAll.forEach(a -> {
+			List<Integer> lan = a.getLan();
+			lan.forEach(b -> map.compute(b, (k, v) -> {
+				Map<Integer, Integer> count = new HashMap<>();
+				if (v != null) {
+					count.putAll(v);
+				}
+				List<Integer> hong = a.getHong();
+				hong.forEach(c -> count.compute(c, (d, f) -> {
+					if (f == null) {
+						return 1;
+					}
+					return ++f;
+				}));
+				return count;
+			}));
+		});
+
+		Map<Integer, Integer> count = new HashMap<>();
+		listAll.forEach(o -> count.compute(o.getLan().get(0), (a, b) -> {
+			if (b == null) {
+				return 1;
+			}
+			return ++b;
+		}));
+
+		Map<Integer, Integer> l1 = map.get(3);
+
+		System.out.println(1);
+	}
+
+	public static class ExcelReadA {
+    	private Integer qishu;
+    	private List<Integer> hong = new ArrayList<>();
+    	private List<Integer> lan = new ArrayList<>();
+
+		public Integer getQishu() {
+			return qishu;
+		}
+
+		public void setQishu(Integer qishu) {
+			this.qishu = qishu;
+		}
+
+		public List<Integer> getHong() {
+			return hong;
+		}
+
+		public void addHong(Integer hong) {
+			this.hong.add(hong);
+		}
+
+		public List<Integer> getLan() {
+			return lan;
+		}
+
+		public void addLan(Integer lan) {
+			this.lan.add(lan);
+		}
+	}
 }
